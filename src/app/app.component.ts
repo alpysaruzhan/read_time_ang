01dfd8f9af9701405838523cb105d9023b1ec27c
alpysaruzhan <alpysaruzan4@gmail.com>
2022-07-29 16:11:05 +0600

    readtime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'read_time_ang';
+  readTime!: string;
+  onReadTimeCalculated(readTimeStr: string) {
+    this.readTime = readTimeStr;
+  } 
 }
